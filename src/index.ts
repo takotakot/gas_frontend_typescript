ba6f411fe6d71a381272d7d8dfaf2891fb226274
takotakot <takotakot@users.noreply.github.com>
2024-08-12 18:48:05 +0900

    feat: Add static HTML dialog

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function onOpen(): void {
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu('Menu').addItem('Open Dialog', openDialog.name).addToUi();
+  ui.createMenu('Menu')
+    .addItem('Open Dialog', openDialog.name)
+    .addItem('Open HTML Dialog', openHtmlDialog.name)
+    .addToUi();
 }
 
 /**
@@ -14,4 +17,14 @@ function onOpen(): void {
 function openDialog(): void {
   const html = HtmlService.createHtmlOutput('<p>Hello</p>');
   SpreadsheetApp.getUi().showModalDialog(html, 'Hello');
+}
+
+/**
+ * Opens an HTML dialog.
+ */
+function openHtmlDialog(): void {
+  const html = HtmlService.createHtmlOutputFromFile(
+    'static/sample-dialog.html'
+  );
+  SpreadsheetApp.getUi().showModalDialog(html, 'Sample');
 }
